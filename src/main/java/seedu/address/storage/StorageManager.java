2f7b99b57f2cea847b9b4259d0806dc36e5e1c32
Chua Wei Wen <weiwen.chua5@gmail.com>
2017-09-29 16:15:59 +0800

    Updated style

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -90,7 +90,7 @@ public class StorageManager extends ComponentManager implements Storage {
     }
 
     @Override
-    public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException{
+    public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
         saveAddressBook(addressBook, addressBookStorage.getAddressBookFilePath() + "-backup.xml");
     }
 

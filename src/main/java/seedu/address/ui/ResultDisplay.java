3112095737bfabeccc655ef1a3a4b3a60d133ede
EDWIN-DELL\Edwin <edwingohhaoyu@gmail.com>
2017-10-01 02:57:10 +0800

    UI Component Enhancement Part 2

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -7,6 +7,7 @@ import com.google.common.eventbus.Subscribe;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
@@ -20,6 +21,8 @@ public class ResultDisplay extends UiPart<Region> {
 
     private static final Logger logger = LogsCenter.getLogger(ResultDisplay.class);
     private static final String FXML = "ResultDisplay.fxml";
+
+    private static final String ERROR_STYLE_CLASS = "error";
 
     private final StringProperty displayed = new SimpleStringProperty("");
 
@@ -36,6 +39,27 @@ public class ResultDisplay extends UiPart<Region> {
     private void handleNewResultAvailableEvent(NewResultAvailableEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         Platform.runLater(() -> displayed.setValue(event.message));
+
+        if(event.isError){
+            setStyleToIndicateCommandFailure();
+        }
+        else{
+            setStyleToDefault();
+        }
+    }
+
+    private void setStyleToDefault(){
+        resultDisplay.getStyleClass().remove(ERROR_STYLE_CLASS);
+    }
+
+    private void setStyleToIndicateCommandFailure(){
+        ObservableList<String> styleClass = resultDisplay.getStyleClass();
+
+        if(styleClass.contains(ERROR_STYLE_CLASS)){
+            return;
+        }
+        else
+            styleClass.add(ERROR_STYLE_CLASS);
     }
 
 }

c6d1c045dfbf4bca5f77ccd3d015df2bbb234379
EDWIN-DELL\Edwin <edwingohhaoyu@gmail.com>
2017-10-10 17:34:33 +0800

    Formatting

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -2,14 +2,15 @@ package seedu.address.logic.commands;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.UnsortedPersons.getUnsortedAddressBook;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.UnsortedPersons.getUnsortedAddressBook;
 
 public class SortCommandTest {
     private Model model;

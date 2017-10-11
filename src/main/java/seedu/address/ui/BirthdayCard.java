72fe387a3b27f6c6a6213519a753052ea0fc18b4
Justin Poh <e0031499@u.nus.edu>
2017-10-11 22:11:18 +0800

    Some tidying up of import statements.

--- src/main/java/seedu/address/ui/BirthdayCard.java
+++ src/main/java/seedu/address/ui/BirthdayCard.java
@@ -6,7 +6,6 @@ import java.util.Random;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.ReadOnlyPerson;

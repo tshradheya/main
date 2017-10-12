024ea7bdf0804ad47a8c61b344ca3142a17867f6
Chua Wei Wen <weiwen.chua5@gmail.com>
2017-10-12 21:28:00 +0800

    Added NameAndTagsContainsKeywordsPredicate.java

---
+++ src/main/java/seedu/address/model/person/NameAndTagsContainsKeywordsPredicate.java
@@ -0,0 +1,71 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
+ */
+public class NameAndTagsContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
+    private final List<String> nameKeywords;
+    private final List<String> tagKeywords;
+
+    public NameAndTagsContainsKeywordsPredicate(List<String> nameKeywords, List<String> tagKeywords) {
+        this.nameKeywords = nameKeywords;
+        this.tagKeywords = tagKeywords;
+    }
+
+    @Override
+    public boolean test(ReadOnlyPerson person) {
+        boolean tagFound = false;
+
+        int numTagKeywords = tagKeywords.size();
+        int tagsMatchedCount = 0;
+
+        if (!tagKeywords.isEmpty()) {
+            Set<Tag> tagsOfPerson = person.getTags();
+            for (Tag personTag : tagsOfPerson) {
+                for (String findTag : tagKeywords) {
+                    if (personTag.tagName.equalsIgnoreCase(findTag)) {
+                        tagsMatchedCount++;
+                    }
+                }
+            }
+        }
+
+        if (tagsMatchedCount == numTagKeywords) {
+            tagFound = true;
+        }
+
+        boolean nameFound = false;
+        if (!nameKeywords.isEmpty()) {
+            nameFound = nameKeywords.stream().anyMatch(nameKeywords -> StringUtil.
+                    containsWordIgnoreCase(person.getName().fullName, nameKeywords));
+        }
+
+        if (nameKeywords.isEmpty() && tagKeywords.isEmpty()) {
+            assert false : "Either name or tag must be non-empty";
+        } else if (nameKeywords.isEmpty()) {
+            return tagFound;
+        } else if (tagKeywords.isEmpty()) {
+            return nameFound;
+        }
+
+        return nameFound && tagFound;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof NameAndTagsContainsKeywordsPredicate // instanceof handles nulls
+                && this.nameKeywords.equals(((NameAndTagsContainsKeywordsPredicate) other)
+                .nameKeywords)
+                && this.tagKeywords.equals(((NameAndTagsContainsKeywordsPredicate) other)
+                .tagKeywords)); // state check
+    }
+
+}

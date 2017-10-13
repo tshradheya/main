d8dd89f84f1e0f64f15814ab036f9eb5cfe4d3cf
Chua Wei Wen <weiwen.chua5@gmail.com>
2017-10-13 19:49:22 +0800

    Removed unused imports

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -1,20 +1,16 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 
 import seedu.address.logic.commands.FilterCommand;
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.person.NameAndTagsContainsKeywordsPredicate;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 public class FilterCommandParserTest {
 

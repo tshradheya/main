c700169098dd1450b3e0153441a79b9626705a77
Chua Wei Wen <weiwen.chua5@gmail.com>
2017-10-13 18:51:52 +0800

    Added logic to FilterCommand

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -1,8 +1,7 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.NameAndTagsContainsKeywordsPredicate;
 
 /**
  * Finds and lists all persons in address book whose name and/or tags contains any of the argument keywords.
@@ -13,14 +12,27 @@ public class FilterCommand extends Command {
     public static final String COMMAND_WORD = "filter";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names and tags contain"
-            + "any of the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "any of the specified keywords and displays them as a list with index numbers.\n"
             + "Parameters: [n/NAME] [t/TAG]...\n"
-            + "Example: " + COMMAND_WORD + " n/Alex t/Friends";
+            + "Note: At least one of the parameters must be specified.\n"
+            + "Example: " + COMMAND_WORD + " n/Alex t/friends";
 
-    //private final NameContainsKeywordsPredicate predicate;
+    private final NameAndTagsContainsKeywordsPredicate predicate;
+
+    public FilterCommand(NameAndTagsContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
 
     @Override
     public CommandResult execute() throws CommandException {
-        throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FilterCommand // instanceof handles nulls
+                && this.predicate.equals(((FilterCommand) other).predicate)); // state check
     }
 }

7fbae00d0baea98a0cada543421d22b0fc552c93
Shradheya Thakre <tshradheya@gmail.com>
2017-10-20 13:13:21 +0800

    Code Refactoring

--- src/test/java/seedu/address/storage/ReadAndStoreImageTest.java
+++ src/test/java/seedu/address/storage/ReadAndStoreImageTest.java
@@ -14,7 +14,7 @@ import seedu.address.logic.parser.exceptions.ImageException;
 
 public class ReadAndStoreImageTest {
 
-    //private static final String IMAGE_NAME = "testDisplaypic";
+    private static final String IMAGE_NAME = "testDisplaypic";
     private static final String INVALID_IMAGE_NAME = "testDisplaypicWrong";
     private static final String VALID_EMAIL = "something@example.com";
 
@@ -25,7 +25,7 @@ public class ReadAndStoreImageTest {
     @Test
     public void test_execute() throws IOException {
 
-        String initialPath = new File("./src/test/resources/pictures/testDisplaypic.jpg").getAbsolutePath();
+        String initialPath = new File("./src/test/resources/pictures/" + IMAGE_NAME + ".jpg").getAbsolutePath();
 
 
         ReadAndStoreImage readAndStoreImage = new ReadAndStoreImage();

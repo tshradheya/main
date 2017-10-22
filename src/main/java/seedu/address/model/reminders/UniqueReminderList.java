a6f0bc3019ebb0153d5d9016bc542fe85fcbca04
Justin Poh <e0031499@u.nus.edu>
2017-10-22 15:34:14 +0800

    Created Reminder, DueDate, UniqueReminderList. Created relevant exceptions DuplicateReminderException and ReminderNotFoundException

---
+++ src/main/java/seedu/address/model/reminders/UniqueReminderList.java
@@ -0,0 +1,101 @@
+package seedu.address.model.reminders;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.model.reminders.exceptions.DuplicateReminderException;
+import seedu.address.model.reminders.exceptions.ReminderNotFoundException;
+import seedu.address.storage.XmlSerializableReminders;
+
+/**
+ * A list of reminders that enforces uniqueness between its elements and does not allow nulls.
+ *
+ * Supports a minimal set of list operations.
+ *
+ * @see Reminder#equals(Object)
+ * @see CollectionUtil#elementsAreUnique(Collection)
+ */
+public class UniqueReminderList implements Iterable<Reminder> {
+    private final ObservableList<Reminder> internalList = FXCollections.observableArrayList();
+
+    public UniqueReminderList() {
+    }
+
+    /**
+     * Constructor used for loading reminder from storage file into program.
+     */
+    public UniqueReminderList(XmlSerializableReminders xmlReminders) {
+        requireNonNull(xmlReminders);
+        try {
+            setReminders(xmlReminders.toModelType());
+        } catch (DuplicateReminderException dre) {
+            assert false : "Reminders from storage should not have duplicates";
+        }
+    }
+
+    public int size() {
+        return internalList.size();
+    }
+
+    /**
+     * Returns true if the list contains an equivalent reminder as the given argument.
+     */
+    public boolean contains(Reminder toCheck) {
+        requireNonNull(toCheck);
+        return internalList.contains(toCheck);
+    }
+
+    /**
+     * Adds a reminder to the list.
+     * @throws DuplicateReminderException if the reminder to add is a duplicate of an existing reminder in the list.
+     */
+    public void add(Reminder toAdd) throws DuplicateReminderException {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicateReminderException();
+        }
+        internalList.add(toAdd);
+    }
+    /**
+     * Removes the equivalent reminder from the list.
+     *
+     * @throws ReminderNotFoundException if no such reminder could be found in the list.
+     */
+    public boolean remove(Reminder toRemove) throws ReminderNotFoundException {
+        requireNonNull(toRemove);
+        final boolean reminderFoundAndDeleted = internalList.remove(toRemove);
+        if (!reminderFoundAndDeleted) {
+            throw new ReminderNotFoundException();
+        }
+        return reminderFoundAndDeleted;
+    }
+
+    public void setReminders(UniqueReminderList replacement) {
+        this.internalList.setAll(replacement.internalList);
+    }
+
+    public void setReminders(List<Reminder> reminders) throws DuplicateReminderException {
+        final UniqueReminderList replacement = new UniqueReminderList();
+        for(final Reminder reminder : reminders) {
+            replacement.add(new Reminder(reminder));
+        }
+        setReminders(replacement);
+    }
+
+    /**
+     * Returns the list as an unmodiafiable {@code ObservableList}.
+     */
+    public ObservableList<Reminder> asObservableList() {
+        return FXCollections.unmodifiableObservableList(internalList);
+    }
+
+    @Override
+    public Iterator<Reminder> iterator() {
+        return internalList.iterator();
+    }
+}

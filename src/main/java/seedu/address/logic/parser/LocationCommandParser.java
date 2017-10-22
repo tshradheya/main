028f80bc8c85191528933e0460c26c57cde21409
Shradheya Thakre <tshradheya@gmail.com>
2017-10-22 14:37:25 +0800

    Parser now handles Command input

--- src/main/java/seedu/address/logic/parser/LocationCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LocationCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.DisplayPictureCommand;
 import seedu.address.logic.commands.LocationCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 

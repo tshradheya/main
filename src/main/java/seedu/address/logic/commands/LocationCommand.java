ca47bbeae23d26f8c5471f311378563c4b5e1c23
Shradheya Thakre <tshradheya@gmail.com>
2017-10-22 14:29:48 +0800

    Implemented command execution

---
+++ src/main/java/seedu/address/logic/commands/LocationCommand.java
@@ -0,0 +1,58 @@
+package seedu.address.logic.commands;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+
+/**
+ * Shows location in Google maps of the specified person
+ */
+public class LocationCommand extends Command {
+
+    public static final String COMMAND_WORD = "location";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays  the location of specified person. "
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_FIND_LOCATION_SUCCESS = "Location of %1$s: %2$s";
+
+    private final Index index;
+
+    public LocationCommand(Index index) {
+        this.index = index;
+    }
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyPerson personWhoseLocationIsToBeShown = lastShownList.get(index.getZeroBased());
+
+        try {
+            model.findLocation(personWhoseLocationIsToBeShown);
+        } catch (PersonNotFoundException pnfe) {
+            assert false : "The target person cannot be missing";
+        }
+
+        return new CommandResult(String.format(MESSAGE_FIND_LOCATION_SUCCESS,
+                personWhoseLocationIsToBeShown.getName().fullName, personWhoseLocationIsToBeShown.getAddress()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof LocationCommand // instanceof handles nulls
+                && this.index.equals(((LocationCommand) other).index)); // state check
+    }
+
+
+}

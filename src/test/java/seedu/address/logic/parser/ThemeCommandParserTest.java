c6321c0a2466f8d290352dc6a831e2ce32a9ab27
Chua Wei Wen <weiwen1993@gmail.com>
2017-10-23 16:30:26 +0800

    Update ThemeCommandParserTest with Exception

--- src/test/java/seedu/address/logic/parser/ThemeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ThemeCommandParserTest.java
@@ -14,19 +14,19 @@ public class ThemeCommandParserTest {
     private ThemeCommandParser parser = new ThemeCommandParser();
 
     @Test
-    public void parse_existingArgs_returnsThemeCommand() {
+    public void parse_existingArgs_returnsThemeCommand() throws Exception {
         ThemeCommand expectedCommand = new ThemeCommand(new Theme(ThemeNames.THEME_DARK, ThemeNames.THEME_DARK_CSS));
         assertParseSuccess(parser, ThemeNames.THEME_DARK, expectedCommand);
     }
 
     @Test
-    public void parse_nonExistingArgs_throwsParseException() {
+    public void parse_nonExistingArgs_throwsParseException() throws Exception {
         assertParseFailure(parser, "unknown_theme", String.format(MESSAGE_UNKNOWN_THEME,
                 ThemeCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void parse_missingArgs_throwsParseException() {
+    public void parse_missingArgs_throwsParseException() throws Exception {
         assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 ThemeCommand.MESSAGE_USAGE));
     }

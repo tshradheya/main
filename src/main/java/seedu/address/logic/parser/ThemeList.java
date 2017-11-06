a6fee9e1b082a889b37e40178562ff7c0100c96c
Chua Wei Wen <weiwen.chua5@gmail.com>
2017-11-06 21:44:24 +0800

    Update theme

---
+++ src/main/java/seedu/address/logic/parser/ThemeList.java
@@ -0,0 +1,20 @@
+//@@author chuaweiwen
+package seedu.address.logic.parser;
+
+/**
+ * List of available themes
+ */
+public class ThemeList {
+    public static final String DEFAULT_PATH = "view/";
+
+    public static final String THEME_DARK = "dark";
+    public static final String THEME_SKY = "sky";
+    public static final String THEME_DAY = "day";
+    public static final String THEME_NIGHT = "night";
+
+    public static final String THEME_DARK_PATH = DEFAULT_PATH + "DarkTheme.css";
+    public static final String THEME_SKY_PATH = DEFAULT_PATH + "SkyTheme.css";
+    public static final String THEME_DAY_PATH = DEFAULT_PATH + "DayTheme.css";
+    public static final String THEME_NIGHT_PATH = DEFAULT_PATH + "NightTheme.css";
+}
+//@@author
